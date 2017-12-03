1b9e04018738559b238c6ccba8f95e0242863a8b
DanielATV <daniel.torov@sansano.usm.cl>
2017-12-03 15:44:07 -0400

    Update quimica.scm

--- quimica.scm
+++ quimica.scm
@@ -9,17 +9,30 @@
                "dubnio" "seaborgio" "bohrio" "hasio" "meitnerio"
                "darmstatio" "roentgenio" "copernicio"))
 
+#|
+Funcion: quimica
+Descripcion: Dado un elemento de partida, busca  un elemento en elementos que empieze con la penultima letra del elemento de partida, repite este
+proceso hasta que no encuentre uno distinto a los que lleva.
+Parametros:
+x: Nombre del elemento para empezar la busqueda.
+Retorno: Lista con todos los elementos que cumplan la condicion.
+|#
+
+
 (define quimica
   (lambda (x)
     (let aux ((buscar x)(rsp (list x))(elem elementos))
+      #|Condicion de termino|#
       (if (null? elem) rsp
+          #|Revisa que no se repitan|#
           (if (not (member (car elem) rsp))
+              #|Aplica la condicion|#
               (if (eqv? (string-ref buscar (- (string-length buscar) 2))(string-ref (car elem) 0))
                   (aux (car elem) (append rsp (list (car elem))) elementos)
+                  #|Si no la cumple sigue con el resto|#
                   (aux buscar rsp (cdr elem))
               )
+              #|Si esta ya esta en la lista sigue con el resto|#
               (aux buscar rsp (cdr elem)))))))
 
-(quimica "molibdeno")
-(quimica "oro")
-              
+
