8d75d273cf7f71091bc6b8f410b3bbb4de05b280
DanielATV <daniel.torov@sansano.usm.cl>
2017-12-03 16:35:37 -0400

    Update raiz.scm

--- raiz.scm
+++ raiz.scm
@@ -6,12 +6,13 @@ Descripcion: Aproxima el resultado de una raiz cuadrada.
 Parametros:
 x entero
 y entero
-Retorno: Resultado de aplicar la aproximacion y veces.
-|#
+Retorno: Resultado de aplicar la aproximacion a x y veces.
+#||#
 
 
 (define raiz
   (lambda (x y)
+    #|Metedo auxiliar que va guardando el resultado|#
     (let aux((num x)(ite 0) (llevo (/ x 2.0)))
       (if (= y ite) llevo
           (aux num (+ ite 1) (* 0.5 (+ llevo (/ num llevo))))))))
