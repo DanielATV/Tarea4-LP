5236f19c8e8a177bb4e0901c22231e87628e1ea3
DanielATV <daniel.torov@sansano.usm.cl>
2017-12-03 15:46:27 -0400

    Update operar.scm

--- operar.scm
+++ operar.scm
@@ -1,14 +1,39 @@
 #lang scheme
+#|
+Funcion: Suma_Enteros
+Descripcion: suma dos enteros positivos
+Parametros:
+n1 entero
+n2 entero
+Retorno: resultado de la operacion aritmetica de la suma entero
+|#
 
 (define codificar
   (lambda (x)
     (if (eqv? x 1)#t
         #f)))
+#|
+Funcion: Suma_Enteros
+Descripcion: suma dos enteros positivos
+Parametros:
+n1 entero
+n2 entero
+Retorno: resultado de la operacion aritmetica de la suma entero
+|#
 
 (define decodificar
   (lambda (x)
     (if (eqv? x #t)1
         0)))
+
+#|
+Funcion: Suma_Enteros
+Descripcion: suma dos enteros positivos
+Parametros:
+n1 entero
+n2 entero
+Retorno: resultado de la operacion aritmetica de la suma entero
+|#
 
 (define operar
   (lambda (x)
